4a5bae489a9c15e055d17a19eccd6702a11e882d
pnickl <ptrnickl@gmail.com>
2020-07-17 17:21:58 +0200

    update sarcos script

--- examples/sarcos.py
+++ examples/sarcos.py
@@ -8,16 +8,16 @@ from sklearn.metrics import explained_variance_score, mean_squared_error, r2_sco
 
 #%%
 
-N_train = 500
+N_train = 400
 N_test = 100
-D = 21
+D_out = 21
 
 seed = 411
 np.random.seed(seed)
 
 #%%
 
-def generate_sarcos_data(D, N_train, N_test):
+def load_sarcos_data(D, N_train, N_test):
     import scipy as sc
     from scipy import io
 
@@ -32,22 +32,16 @@ def generate_sarcos_data(D, N_train, N_test):
     # scale data
     from sklearn.decomposition import PCA
     input_scaler = PCA(n_components=21, whiten=True)
-    target_scaler = PCA(n_components=7, whiten=True)
+    target_scaler = PCA(n_components=1, whiten=True)
 
     input_scaler.fit(data[:, :21])
-    target_scaler.fit(data[:, 21:28])
+    target_scaler.fit(data[:, 21:22])
 
     train_data = {'input': input_scaler.transform(_train_data[:N_train, :21]),
-                  'target': target_scaler.transform(_train_data[:N_train, 21:28])}
+                  'target': target_scaler.transform(_train_data[:N_train, 21:22])}
 
     test_data = {'input': input_scaler.transform(_test_data[:N_test, :21]),
-                 'target': target_scaler.transform(_test_data[:N_test, 21:28])}
-
-    # train_input = train_data[:N_train, :D]
-    # train_target = train_data[:N_train, D]
-    #
-    # test_input = test_data[:N_test, :D]
-    # test_target = test_data[:N_test, D]
+                 'target': target_scaler.transform(_test_data[:N_test, 21:22])}
 
     train_input = train_data['input']
     train_target = train_data['target']
@@ -59,35 +53,39 @@ def generate_sarcos_data(D, N_train, N_test):
 
 #%%
 
-opt = Options(D)
+opt = Options(D_out)
 opt.activ_thresh = 0.3
-opt.max_num_lm = 1000
-opt.max_iter = 500
+opt.max_num_lm = 400
+opt.max_iter = 1000
+
+# opt.init_eta = 1
+# opt.init_lambda = 0.9
+
 opt.print_options()
 
 #%%
 
-X_train, Y_train, X_test, Y_test = generate_sarcos_data(D, N_train, N_test)
+X_train, Y_train, X_test, Y_test = load_sarcos_data(D_out, N_train, N_test)
 Y_train, Y_test = np.reshape(Y_train, (N_train, 1)), np.reshape(Y_test, (N_test, 1))
 
-model = LGR(opt, D)
+model = LGR(opt, D_out)
 debug = False
 model.initialize_local_models(X_train)
 initial_local_models = model.get_local_model_activations(X_train)
 
 nmse = model.run(X_train, Y_train, opt.max_iter, debug)
-print("final nmse (train): {}".format(nmse[-1]))
+print("nmse (train): {}".format(nmse[-1]))
 
 #%%
 
 Yp = model.predict(X_test)
 final_local_models = model.get_local_model_activations(X_test)
-print('Number of data and local models:', final_local_models.shape)
+print('Number of test data and final local models:', final_local_models.shape)
 
 #%%
-test_mse = mean_squared_error(Y_train, Yp)
-test_smse = 1. - r2_score(Y_train, Yp, multioutput='variance_weighted')
-test_evar = explained_variance_score(Y_train, Yp, multioutput='variance_weighted')
+test_mse = mean_squared_error(Y_test, Yp)
+test_smse = 1. - r2_score(Y_test, Yp, multioutput='variance_weighted')
+test_evar = explained_variance_score(Y_test, Yp, multioutput='variance_weighted')
 
 print('FINAL - TEST - MSE:', test_mse, 'SMSE:', test_smse, 'EVAR:', test_evar)
 
